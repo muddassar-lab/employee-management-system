224cdc19bed190d23113f08c5e27d5e47f8fd711
msh-dev <muddassarsaleem89@gmail.com>
2024-08-18 21:03:01 +0500

    Added million lint

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
+import MillionLint from "@million/lint";
 import react from "@vitejs/plugin-react";
 import laravel from "laravel-vite-plugin";
 import { defineConfig } from "vite";
-
+const _plugins = [
+	laravel({
+		input: "resources/js/app.tsx",
+		refresh: true,
+	}),
+	react(),
+];
+_plugins.unshift(MillionLint.vite());
 export default defineConfig({
-	plugins: [
-		laravel({
-			input: "resources/js/app.tsx",
-			refresh: true,
-		}),
-		react(),
-	],
+	plugins: _plugins,
 	server: {
 		host: "employee-management-system.test",
 	},
