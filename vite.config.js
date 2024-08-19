3af303f46928c8bf7fed290f37173a060d326553
msh-dev <muddassarsaleem89@gmail.com>
2024-08-19 15:34:25 +0500

    Added biome configuration

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
-import MillionLint from "@million/lint";
-import react from "@vitejs/plugin-react";
-import laravel from "laravel-vite-plugin";
-import { defineConfig } from "vite";
+import MillionLint from '@million/lint'
+import react from '@vitejs/plugin-react'
+import laravel from 'laravel-vite-plugin'
+import { defineConfig } from 'vite'
+
 const _plugins = [
 	laravel({
-		input: "resources/js/app.tsx",
-		refresh: true,
+		input: 'resources/js/app.tsx',
+		refresh: true
 	}),
 	react(),
-];
-_plugins.unshift(MillionLint.vite());
+	MillionLint.vite()
+]
+
 export default defineConfig({
 	plugins: _plugins,
 	server: {
-		host: "employee-management-system.test",
-	},
-});
+		host: 'employee-management-system.test'
+	}
+})
